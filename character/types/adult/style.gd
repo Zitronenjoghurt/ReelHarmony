5d6bfc730993bb55d01fe800534e0873c94b1ce3
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-07-13 21:01:38 +0200

    add shirt layer and 2 types of shirts

--- character/types/adult/style.gd
+++ character/types/adult/style.gd
@@ -2,13 +2,30 @@ class_name AdultStyle
 extends Resource
 
 enum Body {
-	LIGHT
+	LIGHT,
+	NONE
+}
+
+enum Shirt {
+	DARK_BLUE,
+	LIGHT_BLUE,
+	NONE
 }
 
 @export var bodies: Array[SpriteFrames] = []
+@export var shirts: Array[SpriteFrames] = []
 
-func build_sprites(body: Body) -> Array[AnimatedSprite2D]:
-	var body_sprite = AnimatedSprite2D.new()
-	body_sprite.sprite_frames = bodies[body]
+func build_sprites(body: Body, shirt: Shirt) -> Array[AnimatedSprite2D]:
+	var sprites: Array[AnimatedSprite2D] = []
 	
-	return [body_sprite]
+	if body < len(bodies):
+		var body_sprite = AnimatedSprite2D.new()
+		body_sprite.sprite_frames = bodies[body]
+		sprites.append(body_sprite)
+	
+	if shirt < len(shirts):
+		var shirt_sprite = AnimatedSprite2D.new()
+		shirt_sprite.sprite_frames = shirts[shirt]
+		sprites.append(shirt_sprite)
+	
+	return sprites
--- character/types/adult/style_config.gd
+++ character/types/adult/style_config.gd
@@ -4,7 +4,8 @@ extends CharacterStyleConfig
 const style_path = "res://resources/character/adult/style.tres"
 
 @export var body: AdultStyle.Body = AdultStyle.Body.LIGHT
+@export var shirt: AdultStyle.Shirt = AdultStyle.Shirt.NONE
 
 func build_sprites() -> Array[AnimatedSprite2D]:
 	var style = ResourceLoader.load(style_path) as AdultStyle
-	return style.build_sprites(body)
+	return style.build_sprites(body, shirt)
